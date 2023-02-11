798c87df58dfae4b660980d9cfa4a67d2ef844e9
Su Hlaing <suhlaing200341@gmail.com>
2023-02-11 09:35:39 +0630

    Added to node-express files

---
+++ api/index.ts
@@ -0,0 +1,40 @@
+import express, { Request, Response } from 'express';
+
+import dotenv from 'dotenv';
+dotenv.config();
+const API = process.env.Api_Url;
+
+const app = express();
+
+app.use(express.static('public'));
+
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="shortcut icon" href="#"/>
+</head>
+<body>
+    <h1>hello world</h1>
+    <script type="text/javascript">
+      localStorage.setItem("apiUrl", "${API}")
+      window.location.href = "/"
+    </script>
+</body>
+</html>
+`
+app.get('/api/users', (req:Request, res:Response) => {
+    res.send({name: "Alex", age: 20, email: "alex@gmail.com"})
+})
+
+app.get('/api', (req:Request, res:Response) => {
+    res.send(html)
+})
+
+app.listen(3000, () => {
+    console.log("Server started: on port 3000")
+})
